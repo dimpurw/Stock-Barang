544e6a4f3c24fc3161c908ee171f80edec9ec935
dimpurw <terminatorid2@gmail.com>
2021-05-12 13:03:37 +0700

    db stock barang

---
+++ database/migrations/2021_05_12_023359_create_stock_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang');
+            $table->string('jenis');
+            $table->string('merk');
+            $table->char('ukuran');
+            $table->integer('stock');
+            $table->string('satuan');
+            $table->string('lokasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_items');
+    }
+}
