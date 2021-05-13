d870317079a384dde8869ad79ab83b3e6ed4b6e3
dimpurw <terminatorid2@gmail.com>
2021-05-14 01:00:47 +0700

    update model

---
+++ app/ExitItems.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExitItems extends Model
+{
+    protected $table = 'exit_items';
+    protected $guarded = [];
+}
---
+++ app/IncomingItems.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IncomingItems extends Model
+{
+    protected $table = 'incoming_items';
+    protected $guarded = [];
+}
