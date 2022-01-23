58d248c0b22d8a575980f26ceddc71da8cabfea7
dimpurw <terminatorid2@gmail.com>
2022-01-23 11:42:46 +0700

    update db

--- database/migrations/2021_08_01_035447_create_items_table.php
+++ database/migrations/2021_08_01_035447_create_items_table.php
@@ -16,6 +16,7 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('unit_id')->unsigned();
+            $table->bigInteger('kode_barang')->unique();
             $table->string('nama_barang');
             $table->integer('harga');
             $table->foreign('unit_id')->references('id')->on('units')->onDelete('cascade');
