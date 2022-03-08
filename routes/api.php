d493d73d36c01da8e440b49e065bf34ab0242f33
dimpurw <terminatorid2@gmail.com>
2022-03-08 21:37:54 +0700

    api auth

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', 'API\AuthController@register');
+
+Route::post('/login', 'API\AuthController@login');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    Route::post('/logout', 'API\AuthController@logout');
 });
